0548dcac6be9eaa7ddae9a097de2569a3828db1e
Priyanka <priyankakudchi79@gmail.com>
2026-05-13 02:26:20 +0530

    Fixed upload user header

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -41,19 +41,19 @@ export const resumeAPI = {
     form.append("file", file);
     // return client.post("/resume/upload", form);
     return client.post("/resume/upload", form, {
-  headers: {
-    "Content-Type": "multipart/form-data",
-    "X-User-ID": JSON.parse(localStorage.getItem("jat_user"))?.id,
-  },
-});
+      headers: {
+        "Content-Type": "multipart/form-data",
+        "X-User-ID": JSON.parse(localStorage.getItem("jat_user"))?.id,
+      },
+    });
   },
   // list: () => client.get("/resume"),
   list: () =>
-  client.get("/resume/list", {
-    headers: {
-      "X-User-ID": JSON.parse(localStorage.getItem("jat_user"))?.id,
-    },
-  }),
+    client.get("/resume/list", {
+      headers: {
+        "X-User-ID": JSON.parse(localStorage.getItem("jat_user"))?.id,
+      },
+    }),
   get: (id) => client.get(`/resumes/${id}`),
   delete: (id) => client.delete(`/resumes/${id}`),
 };
