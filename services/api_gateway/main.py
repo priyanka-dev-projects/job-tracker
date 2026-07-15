0e0b9b1b38b9c0f34156fa6a1734e7f3e6219e37
Priyanka <priyankakudchi79@gmail.com>
2026-07-15 13:04:29 +0530

    Fix backend URL API

--- services/api_gateway/main.py
+++ services/api_gateway/main.py
@@ -34,7 +34,7 @@ RESUME_PARSER_URL  = os.getenv("RESUME_PARSER_URL", "https://resume-praser-dnlp.
 # JOB_MATCHER_URL    = os.getenv("JOB_MATCHER_URL",   "http://localhost:8002")
 JOB_MATCHER_URL    = os.getenv("JOB_MATCHER_URL",   "https://jat-job-matcher.onrender.com")
 # APP_MANAGER_URL    = os.getenv("APP_MANAGER_URL",    "http://localhost:8003")
-APP_MANAGER_URL    = os.getenv("APP_MANAGER_URL",    "https://app-manager-a3le.onrender.com")
+APP_MANAGER_URL    = os.getenv("APP_MANAGER_URL",    "https://app-manager-4efl.onrender.com")
 
 
 app = FastAPI(title="JAT API Gateway", version="1.0.0")
